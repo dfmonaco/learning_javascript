b75dd678fd1917cbee1972bac984afb219531acf
Diego Mónaco <dfmonaco@gmail.com>
2020-02-13 11:25:18 -0300

    combine redux reducers

--- javascript/redux/store.js
+++ javascript/redux/store.js
@@ -1,3 +1,4 @@
+// CREATESTORE IS THE LIBRARY CODE
 function createStore(reducer) {
   // 1. Keep State
   // 2. Get State
@@ -28,43 +29,87 @@ function createStore(reducer) {
   };
 }
 
+// ACTIONS AND REDUCERS ARE APP CODE
+
+// ACTIONS
+//
+const ADD_NUMBER = 'ADD_NUMBER';
+const REMOVE_NUMBER = 'REMOVE_NUMBER';
+const ADD_STRING = 'ADD_STRING';
+const REMOVE_STRING = 'REMOVE_STRING';
+
 const addNumber = (number) => (
   {
-    type: 'ADD_NUMBER',
+    type: ADD_NUMBER,
     number,
   }
 );
 
 const removeNumber = (number) => (
   {
-    type: 'REMOVE_NUMBER',
+    type: REMOVE_NUMBER,
     number,
   }
 );
 
+const addString = (string) => (
+  {
+    type: ADD_STRING,
+    string,
+  }
+);
+
+const removeString = (string) => (
+  {
+    type: REMOVE_STRING,
+    string,
+  }
+);
+
+// REDUCERS
 // the reducer reduces the current state and an action to a new state
 // it must be a pure function:
 // 1. Always returns the same result given the same arguments
 // 2. Only depend on the arguments passed into them, not on global variables
 // 3. Never produce any side effects.
 //
-const reducer = (state = [], action) => {
+const numbersReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_NUMBER':
+    case ADD_NUMBER:
       return state.concat([action.number]);
-    case 'REMOVE_NUMBER':
+    case REMOVE_NUMBER:
       return state.filter((number) => number !== action.number);
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const stringsReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_STRING:
+      return state.concat([action.string]);
+    case REMOVE_STRING:
+      return state.filter((string) => string !== action.string);
+    default:
+      return state;
+  }
+};
+
+const appReducer = (state = {}, action) => (
+  {
+    numbers: numbersReducer(state.numbers, action),
+    strings: stringsReducer(state.strings, action),
+  }
+);
+
+// USING THE LIBRARY AND APP CODE
+
+const store = createStore(appReducer);
 
 console.log(store.getState());
 
-store.subscribe((state) => { console.log(state+' from subscriber 1'); });
-store.subscribe((state) => { console.log(state+' from subscriber 2'); });
+store.subscribe((state) => { console.log('SUBSCRIBER 1:\n'+JSON.stringify(state, null, 4)); });
+store.subscribe((state) => { console.log('SUBSCRIBER 2:\n'+JSON.stringify(state, null, 4)); });
 
 store.dispatch(addNumber(3));
 store.dispatch(addNumber(8));
@@ -72,4 +117,8 @@ store.dispatch(addNumber(23));
 
 store.dispatch(removeNumber(8));
 
-module.exports = createStore;
+store.dispatch(addString('foo'));
+store.dispatch(addString('bar'));
+store.dispatch(addString('baz'));
+
+store.dispatch(removeString('bar'));
