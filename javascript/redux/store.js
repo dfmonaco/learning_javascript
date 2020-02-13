86a05ac20a5f361d4d51e8d5121613e25c3d243d
Diego Mónaco <dfmonaco@gmail.com>
2020-02-13 09:33:00 -0300

    more redux

--- javascript/redux/store.js
+++ javascript/redux/store.js
@@ -1,10 +1,10 @@
-function createStore() {
+function createStore(reducer) {
   // 1. Keep State
   // 2. Get State
   // 3. Subscribe to State changes (listeners)
   // 4. Update State
 
-  const state = { hello: 'world' };
+  let state;
   let listeners = [];
 
   const getState = () => state;
@@ -16,15 +16,60 @@ function createStore() {
     };
   };
 
-  const execListeners = () => {
-    listeners.forEach((listener) => listener());
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener(state));
   };
 
   return {
     getState,
     subscribe,
-    execListeners,
+    dispatch,
   };
 }
+
+const addNumber = (number) => (
+  {
+    type: 'ADD_NUMBER',
+    number,
+  }
+);
+
+const removeNumber = (number) => (
+  {
+    type: 'REMOVE_NUMBER',
+    number,
+  }
+);
+
+// the reducer reduces the current state and an action to a new state
+// it must be a pure function:
+// 1. Always returns the same result given the same arguments
+// 2. Only depend on the arguments passed into them, not on global variables
+// 3. Never produce any side effects.
+//
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_NUMBER':
+      return state.concat([action.number]);
+    case 'REMOVE_NUMBER':
+      return state.filter((number) => number !== action.number);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.subscribe((state) => { console.log(state+' from subscriber 1'); });
+store.subscribe((state) => { console.log(state+' from subscriber 2'); });
+
+store.dispatch(addNumber(3));
+store.dispatch(addNumber(8));
+store.dispatch(addNumber(23));
+
+store.dispatch(removeNumber(8));
 
 module.exports = createStore;
